5205b0aa16339a22ebd0af6dca84dbd99e3a7bf2
RoiFuries <roi.furies@gmail.com>
2023-03-13 18:20:07 -0600

    create routes

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('', (req, res)=>{
+app.get('/', (req, res)=>{
   res.send('hola mi server en express')
 });
+
+app.get('/nueva-ruta', (req, res)=>{
+  res.send('hola soy una nueva ruta')
+});
+
+app.get('/productos', (req, res)=>{
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+});
+
+app.get('/home', (req, res)=>{
+  res.json('hola perras')
+});
+
 
 app.listen(port, ()=>{
   console.log('mi port ' + port);
